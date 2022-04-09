affc07d6dfb3cf54c3f1292291f53cf55b61f99e
Ricardo Andrés Calvo <rcalvom@unal.edu.co>
2022-04-08 23:26:18 -0500

    Deteccion de bordes

--- border_detection.c
+++ border_detection.c
@@ -46,26 +46,47 @@ void border_detection_filter(char* input_filename, char* output_filename, double
         exit(EXIT_FAILURE);
     }
     /**
-     * @brief Allocate memory for generated image
+     * @brief Allocate memory for generated images
      * 
      */
     size_t image_size = width * height;
+    unsigned char* grayscale_image = malloc(image_size);
     unsigned char* output_image = malloc(image_size);
-    if(output_image == NULL){
+    if(grayscale_image == NULL || output_image == NULL){
         perror("The memory couldn't be allocated. Aborting");
         exit(EXIT_FAILURE);
     }
     /**
-     * @brief Create and execute the threads to generate the image
+     * @brief Define variables to threads
      * 
      */
     pthread_t tfd[threads_count];
     struct border_detection_thread_input data[threads_count];
     for(int i = 0; i < threads_count; i++){
         data[i].input_image = input_image;
+        data[i].grayscale_image = grayscale_image;
         data[i].output_image = output_image;
         data[i].start = image_size / threads_count * i;
         data[i].end = image_size / threads_count * (i + 1);
+        data[i].width = width;
+        data[i].height = height;
+        data[i].filter_intensity = filter_intensity;
+    }
+    /**
+     * @brief Create and execute the threads to grayscale the image
+     * 
+     */
+    for(int i = 0; i < threads_count; i++){
+        pthread_create(&tfd[i], NULL, grayscale_filter_thread, &data[i]);
+    }
+    for(int i = 0; i < threads_count; i++){
+        pthread_join(tfd[i], NULL);
+    }
+    /**
+     * @brief Create and execute the threads to apply border filter the image
+     * 
+     */
+    for(int i = 0; i < threads_count; i++){
         pthread_create(&tfd[i], NULL, border_detection_filter_thread, &data[i]);
     }
     for(int i = 0; i < threads_count; i++){
@@ -86,6 +107,7 @@ void border_detection_filter(char* input_filename, char* output_filename, double
      * 
      */
     stbi_image_free(input_image);
+    free(grayscale_image);
     free(output_image);
 }
 
@@ -102,13 +124,42 @@ unsigned char rbg_to_grayscale(unsigned char r, unsigned char g, unsigned char b
 }
 
 /**
- * @brief Thread Function. Calculate a part of the filter
+ * @brief Thread Function. Calculate grayscale of a section of an image
  * 
  * @param data Thread input pointer
  */
 void *border_detection_filter_thread(void* data){
     struct border_detection_thread_input input = *((struct border_detection_thread_input *)data);
     for(int i = input.start; i < input.end; i++){
-        *(input.output_image + i) = rbg_to_grayscale(*(input.input_image + 3 * i), *(input.input_image + 3 * i + 1), *(input.input_image + 3 * i + 2));
+        int p11 = (i % input.width == 0 || i < input.width) ? 0 : *(input.grayscale_image + i - input.width - 1);
+        int p12 = (i < input.width) ? 0 : *(input.grayscale_image + i - input.width);
+        int p13 = (i % input.width == input.width - 1 || i < input.width) ? 0 : *(input.grayscale_image + i - input.width + 1);
+        int p21 = (i % input.width == 0) ? 0 : *(input.grayscale_image + i - 1);
+        int p22 = *(input.grayscale_image + i);
+        int p23 = (i % input.width == input.width - 1) ? 0 : *(input.grayscale_image + i + 1);
+        int p31 = (i % input.width == 0 || i + input.width >= input.width * input.height) ? 0 : *(input.grayscale_image + i + input.width - 1);
+        int p32 = (i + input.width >= input.width * input.height) ? 0 : *(input.grayscale_image + i + input.width);
+        int p33 = (i % input.width == input.width - 1 || i + input.width >= input.width * input.height) ? 0 : *(input.grayscale_image + i + input.width + 1);
+        int result = (-input.filter_intensity * p11) + (-input.filter_intensity * p12) + (-input.filter_intensity * p13) + (-input.filter_intensity * p21) + (8 * input.filter_intensity * p22) + (-input.filter_intensity * p23) + (-input.filter_intensity * p31) + (-input.filter_intensity * p32) + (-input.filter_intensity * p33);
+        if(result < 0){
+            *(input.output_image + i) = 0;
+        }else if(result > 255){
+            *(input.output_image + i) = 255;
+        }else{
+            *(input.output_image + i) = result;
+        }
+    }
+}
+
+
+/**
+ * @brief Thread Function. Calculate the revolution of an image
+ * 
+ * @param data Thread input pointer
+ */
+void *grayscale_filter_thread(void* data){
+    struct border_detection_thread_input input = *((struct border_detection_thread_input *)data);
+    for(int i = input.start; i < input.end; i++){
+        *(input.grayscale_image + i) = rbg_to_grayscale(*(input.input_image + 3 * i), *(input.input_image + 3 * i + 1), *(input.input_image + 3 * i + 2));
     }
 }
--- border_detection.h
+++ border_detection.h
@@ -4,12 +4,17 @@
 void border_detection_filter(char* input_filename, char* output_filename, double filter_intensity, int threads_count);
 unsigned char rbg_to_grayscale(unsigned char r, unsigned char g, unsigned char b);
 void *border_detection_filter_thread(void* data);
+void *grayscale_filter_thread(void* data);
 
 struct border_detection_thread_input {
     unsigned char* input_image;
+    unsigned char* grayscale_image;
     unsigned char* output_image;
     int start;
     int end;
+    int width;
+    int height;
+    int filter_intensity;
 };
 
 #endif
